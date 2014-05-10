c6145a95986b342db45043b071e096d7249da931
jimmy3310 <coffee.cat.tw@gmail.com>
2014-05-10 14:14:39 +0800

    hw4

--- js/checker.js
+++ js/checker.js
@@ -16,7 +16,7 @@ $.getJSON("http://jsbin.com/jaziroja/1", {}, function(data){
   startButton.removeAttr('disabled').removeClass('disabled');
 });
 
-
+// 設定 Facebook AppID
 window.fbAsyncInit = function(){
   FB.init({
     appId: '1432032383716667', // 若可以，請換成自己的 App ID !
@@ -40,6 +40,8 @@ window.fbAsyncInit = function(){
         results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
       });
     }, {scope: 'user_groups'});
+	    // 比對每個使用者的 group 是否有在 junkGroups 中出現
+		
     // 1. 讓使用者登入此 Facebook App (FB.login)
     // 2. 以 FB.api 拿到使用者的 group 列表
     // 拿到使用者 group 列表的 response 之後：
@@ -47,20 +49,7 @@ window.fbAsyncInit = function(){
   });
 };
 
-// 設定 Facebook AppID
-window.fbAsyncInit = function(){
-  FB.init({
-    appId: '1432032383716667', // 若可以，請換成自己的 App ID !
-    status: true
-  });
 
-  // 比對每個使用者的 group 是否有在 junkGroups 中出現
-  //
-  startButton.click(function(){
-    results.empty(); // 清除結果內容
-    $('.hw4-complete').remove(); // 移除「掃描完成」
 
-   
 
-  });
-};
+ 
